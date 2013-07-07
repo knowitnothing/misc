085c0d149ba7614a8e4f7ec6a739b770cb7929e0
A <B>
2013-07-06 22:14:18 -0300

    A bot that does not require a browser.

---
+++ justdice/no_browser_bot/player.py
@@ -0,0 +1,164 @@
+#
+# Browserless Bot for playing at just-dice.com
+#
+# Create your own strategy by redifining the "play" function.
+#
+
+import sys
+import time
+import random
+import urllib2
+import cookielib
+from decimal import Decimal
+
+from socketio import SocketIO
+
+BASE_DOMAIN = 'just-dice.com'
+BASE_URL = 'https://%s' % BASE_DOMAIN
+
+cj = cookielib.CookieJar()
+def load_justdice():
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders.append(('Origin', BASE_URL))
+
+    opener.open(BASE_URL)
+    req = opener.open('%s/socket.io/1' % BASE_URL)
+    response = req.read()
+    return response
+
+
+class JustDiceSocket(object):
+    def __init__(self, response, login):
+        self.login = login
+        self._setup_sock(response)
+
+    def _setup_sock(self, response):
+        self.wins = 0
+        self.losses = 0
+        self.csrf = None
+        self.user_id = None
+        self.logged_in = False
+        self.waiting_bet_result = False
+        self.last_bet = None
+
+        origin = BASE_URL
+        self.sock = SocketIO(BASE_DOMAIN, origin, response, secure=True)
+
+        self.sock.on('init', self.on_init)
+        self.sock.on('set_hash', self.on_set_hash)
+        self.sock.on('reload', self.on_reload)
+        self.sock.on('result', self.on_result)
+
+        self.sock.on('old_results', lambda *args: None)
+        self.sock.on('chat', lambda *args: None)
+        self.sock.on('wins', lambda *args: None)
+        self.sock.on('losses', lambda *args: None)
+
+
+    def bet(self, win_chance, amount, roll_hi):
+        if self.waiting_bet_result:
+            raise Exception("Still waiting for last bet result")
+        which = 'hi' if roll_hi else 'lo'
+        self.sock.emit('bet', self.csrf, {'chance': '%s' % str(win_chance),
+            'bet': format(amount, '.8f'), 'which': '%s' % which})
+        self.waiting_bet_result = True
+
+    def randomize(self, user_seed=None):
+        # user_seed must be a string.
+        self.sock.emit('random', self.csrf)
+        if user_seed is None:
+            user_seed = str(random.randint(0, int('9' * 24)))
+        self.sock.emit('seed', self.csrf, user_seed, True)
+
+    def on_result(self, result):
+        if result['uid'] == self.user_id:
+            self.last_bet = {
+                    'win': result['win'],
+                    'roll_high': result['high'],
+                    'lucky_number': Decimal(result['lucky']) / 10000}
+            self.waiting_bet_result = False
+
+    def on_reload(self):
+        self.sock.emit('disconnect')
+        response = load_justdice()
+        self._setup_sock(response)
+
+    def on_set_hash(self, new_cookie_hash):
+        for cookie in cj:
+            if cookie.name == 'hash':
+                cookie.value = new_cookie_hash
+                break
+
+    def on_init(self, data):
+        self.csrf = data[u'csrf']
+        self.user_id = data[u'uid']
+        self.house_edge = data[u'edge']
+
+        if data['username'] is None and self.login:
+            self.sock.emit('login', self.csrf,
+                    self.login['user'], self.login['pwd'], '')
+        else:
+            self.logged_in = True
+
+
+def main(user=None, pwd=None, new_seed=True):
+    print "Connecting..."
+    response = load_justdice()
+    login_info = {'user': user, 'pwd': pwd} if user is not None else None
+    justdice = JustDiceSocket(response, login=login_info)
+    sys.stdout.write("Logging in...")
+    sys.stdout.flush()
+    while not justdice.logged_in:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(0.75)
+    print
+
+    if new_seed:
+        print "Generating new server seed.."
+        justdice.randomize('')
+        print
+
+    win, total = play(justdice)
+    print "\nWin ratio: %d/%d = %s" % (win, total, Decimal(win) / total)
+
+    justdice.sock.emit('disconnect')
+
+def roll_dice(justdice, win_chance, amount, roll_hi):
+    roll_for = Decimal('99.9999') - win_chance if roll_hi else win_chance
+    sys.stdout.write("Rolling dice for %s %s ..." % (
+        ">" if roll_hi else "<", format(roll_for, '07.4f')))
+    sys.stdout.flush()
+    justdice.bet(win_chance=win_chance, amount=amount, roll_hi=roll_hi)
+    while justdice.waiting_bet_result:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print " %s %s" % (format(justdice.last_bet["lucky_number"], '07.4f'),
+        "win!" if justdice.last_bet['win'] else "lose :/")
+
+    return justdice.last_bet['win']
+
+
+def play(justdice):
+    # XXX Define your strategy here.
+    win = 0     # Win count.
+    nroll = 40  # Roll n times.
+
+    amount = Decimal('0')     # Amount in BTC to bet.
+    roll_hi = False
+    win_chance = Decimal('2') # %.
+
+    for _ in xrange(nroll):
+        if roll_dice(justdice, win_chance, amount, roll_hi):
+            win += 1
+
+    return win, nroll
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print "WARNING user and password were not specified."
+        print "Expected usage: %s user password" % sys.argv[0]
+        print "***" * 15
+    main(*sys.argv[1:3])
---
+++ justdice/no_browser_bot/socketio.py
@@ -0,0 +1,435 @@
+#
+# This is a modification of the original socketIO_client.__init__.py
+#
+
+import socket
+import websocket
+from time import sleep
+from json import dumps, loads
+from threading import Thread, Event
+
+PROTOCOL = 1  # socket.io protocol version
+
+
+class BaseNamespace(object):  # pragma: no cover
+    'Define socket.io behavior'
+
+    def __init__(self, _socketIO, path):
+        self._socketIO = _socketIO
+        self._path = path
+        self._callbackByEvent = {}
+        self.initialize()
+
+    def initialize(self):
+        'Initialize custom variables here; you can override this method'
+        pass
+
+    def on_connect(self):
+        'Called when socket is connecting; you can override this method'
+        pass
+
+    def on_disconnect(self):
+        'Called when socket is disconnecting; you can override this method'
+        pass
+
+    def on_error(self, reason, advice):
+        'Called when server sends an error; you can override this method'
+        print '[Error] %s' % advice
+
+    def on_message(self, data):
+        'Called when server sends a message; you can override this method'
+        print '[Message] %s' % data
+
+    def on_event(self, event, *args):
+        """
+        Called when server emits an event; you can override this method.
+        Called only if the program cannot find a more specific event handler,
+        such as one defined by namespace.on('my_event', my_function).
+        """
+        callback, args = find_callback(args)
+        arguments = [repr(_) for _ in args]
+        if callback:
+            arguments.append('callback(*args)')
+            callback(*args)
+        #print '[Event] %s(%s)' % (event, ', '.join(arguments))
+
+    def on_open(self, *args):
+        print '[Open]', args
+
+    def on_close(self, *args):
+        print '[Close]', args
+
+    def on_retry(self, *args):
+        print '[Retry]', args
+
+    def on_reconnect(self, *args):
+        print '[Reconnect]', args
+
+    def message(self, data='', callback=None):
+        self._socketIO.message(data, callback, path=self._path)
+
+    def emit(self, event, *args, **kw):
+        kw['path'] = self._path
+        self._socketIO.emit(event, *args, **kw)
+
+    def on(self, event, callback):
+        'Define a callback to handle a custom event emitted by the server'
+        self._callbackByEvent[event] = callback
+
+    def _get_eventCallback(self, event):
+        # Check callbacks defined by on()
+        try:
+            return self._callbackByEvent[event]
+        except KeyError:
+            pass
+        # Check callbacks defined explicitly or use on_event()
+        callback = lambda *args: self.on_event(event, *args)
+        return getattr(self, 'on_' + event.replace(' ', '_'), callback)
+
+
+class SocketIO(object):
+
+    def __init__(self, host, origin, response, secure=False):
+        """
+        Create a socket.io client that connects to a socket.io server
+        at the specified host and port.  Set secure=True to use HTTPS / WSS.
+        """
+        self._socketIO = _SocketIO(host, secure, response, origin)
+        self._namespaceByPath = {}
+        self.define(BaseNamespace)  # Define default namespace
+
+        self._rhythmicThread = _RhythmicThread(
+            self._socketIO.heartbeatInterval,
+            self._socketIO.send_heartbeat)
+        self._rhythmicThread.start()
+
+        self._listenerThread = _ListenerThread(
+            self._socketIO,
+            self._namespaceByPath)
+        self._listenerThread.start()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+
+    def __del__(self):
+        self.disconnect(close=False)
+
+    @property
+    def connected(self):
+        return self._socketIO.connected
+
+    def disconnect(self, path='', close=True):
+        if self.connected:
+            self._socketIO.disconnect(path, close)
+        if path:
+            del self._namespaceByPath[path]
+        else:
+            self._rhythmicThread.cancel()
+            self._listenerThread.cancel()
+
+    def define(self, Namespace, path=''):
+        if path:
+            self._socketIO.connect(path)
+        namespace = Namespace(self._socketIO, path)
+        self._namespaceByPath[path] = namespace
+        return namespace
+
+    def get_namespace(self, path=''):
+        return self._namespaceByPath[path]
+
+    def on(self, event, callback, path=''):
+        return self.get_namespace(path).on(event, callback)
+
+    def message(self, data='', callback=None, path=''):
+        self._socketIO.message(data, callback, path)
+
+    def emit(self, event, *args, **kw):
+        self._socketIO.emit(event, *args, **kw)
+
+    def wait(self, seconds=None):
+        if seconds:
+            self._listenerThread.wait(seconds)
+        else:
+            try:
+                while self.connected:
+                    sleep(1)
+            except KeyboardInterrupt:
+                pass
+
+    def wait_for_callbacks(self, seconds=None):
+        self._listenerThread.wait_for_callbacks(seconds)
+
+
+class _RhythmicThread(Thread):
+    'Execute call every few seconds'
+
+    daemon = True
+
+    def __init__(self, intervalInSeconds, call, *args, **kw):
+        super(_RhythmicThread, self).__init__()
+        self.intervalInSeconds = intervalInSeconds
+        self.call = call
+        self.args = args
+        self.kw = kw
+        self.done = Event()
+
+    def run(self):
+        while not self.done.is_set():
+            self.call(*self.args, **self.kw)
+            self.done.wait(self.intervalInSeconds)
+
+    def cancel(self):
+        self.done.set()
+
+
+class _ListenerThread(Thread):
+    'Process messages from socket.io server'
+
+    daemon = True
+
+    def __init__(self, _socketIO, _namespaceByPath):
+        super(_ListenerThread, self).__init__()
+        self._socketIO = _socketIO
+        self._namespaceByPath = _namespaceByPath
+        self.done = Event()
+        self.ready = Event()
+        self.ready.set()
+
+    def cancel(self):
+        self.done.set()
+
+    def wait(self, seconds):
+        self.done.wait(seconds)
+
+    def wait_for_callbacks(self, seconds):
+        self.ready.clear()
+        self.ready.wait(seconds)
+
+    def get_ackCallback(self, packetID):
+        return lambda *args: self._socketIO.ack(packetID, *args)
+
+    def run(self):
+        while not self.done.is_set():
+            try:
+                code, packetID, path, data = self._socketIO.recv_packet()
+            except SocketIOConnectionError, error:
+                print error
+                self.cancel()
+                break
+            except SocketIOPacketError, error:
+                print error
+                continue
+            try:
+                namespace = self._namespaceByPath[path]
+            except KeyError:
+                print 'Received unexpected path (%s)' % path
+                continue
+            try:
+                delegate = {
+                    '0': self.on_disconnect,
+                    '1': self.on_connect,
+                    '2': self.on_heartbeat,
+                    '3': self.on_message,
+                    '4': self.on_json,
+                    '5': self.on_event,
+                    '6': self.on_ack,
+                    '7': self.on_error,
+                }[code]
+            except KeyError:
+                print 'Received unexpected code (%s)' % code
+                continue
+            delegate(packetID, namespace._get_eventCallback, data)
+
+    def on_disconnect(self, packetID, get_eventCallback, data):
+        get_eventCallback('disconnect')()
+
+    def on_connect(self, packetID, get_eventCallback, data):
+        get_eventCallback('connect')()
+
+    def on_heartbeat(self, packetID, get_eventCallback, data):
+        pass
+
+    def on_message(self, packetID, get_eventCallback, data):
+        args = [data]
+        if packetID:
+            args.append(self.get_ackCallback(packetID))
+        get_eventCallback('message')(*args)
+
+    def on_json(self, packetID, get_eventCallback, data):
+        args = [loads(data)]
+        if packetID:
+            args.append(self.get_ackCallback(packetID))
+        get_eventCallback('message')(*args)
+
+    def on_event(self, packetID, get_eventCallback, data):
+        valueByName = loads(data)
+        event = valueByName['name']
+        args = valueByName.get('args', [])
+        if packetID:
+            args.append(self.get_ackCallback(packetID))
+        get_eventCallback(event)(*args)
+
+    def on_ack(self, packetID, get_eventCallback, data):
+        dataParts = data.split('+', 1)
+        messageID = int(dataParts[0])
+        args = loads(dataParts[1]) if len(dataParts) > 1 else []
+        callback = self._socketIO.get_messageCallback(messageID)
+        if not callback:
+            return
+        callback(*args)
+        if not self._socketIO.has_messageCallback:
+            self.ready.set()
+
+    def on_error(self, packetID, get_eventCallback, data):
+        reason, advice = data.split('+', 1)
+        get_eventCallback('error')(reason, advice)
+
+
+class _SocketIO(object):
+    'Low-level interface to remove cyclic references in child threads'
+
+    messageID = 0
+
+    def __init__(self, host, secure, response, origin):
+        baseURL = '%s/socket.io/%s' % (host, PROTOCOL)
+
+        responseParts = response.split(':')
+        sessionID = responseParts[0]
+        heartbeatTimeout = int(responseParts[1])
+        supportedTransports = responseParts[3].split(',')
+        if 'websocket' not in supportedTransports:
+            raise SocketIOError('Could not parse handshake')
+        socketScheme = 'wss' if secure else 'ws'
+        socketURL = '%s://%s/websocket/%s' % (socketScheme, baseURL, sessionID)
+        websock = websocket.WebSocket()
+        websock.connect(socketURL, origin=origin)
+        self.connection = websock
+        self.heartbeatInterval = heartbeatTimeout - 2
+        self.callbackByMessageID = {}
+
+    def __del__(self):
+        self.disconnect(close=False)
+
+    def disconnect(self, path='', close=True):
+        if not self.connected:
+            return
+        if path:
+            self.send_packet(0, path)
+        elif close:
+            self.connection.close()
+
+    def connect(self, path):
+        self.send_packet(1, path)
+
+    def send_heartbeat(self):
+        try:
+            self.send_packet(2)
+        except SocketIOPacketError:
+            print 'Could not send heartbeat'
+            pass
+
+    def message(self, data, callback, path):
+        if isinstance(data, basestring):
+            code = 3
+            packetData = data
+        else:
+            code = 4
+            packetData = dumps(data, ensure_ascii=False)
+        self.send_packet(code, path, packetData, callback)
+
+    def emit(self, event, *args, **kw):
+        callback, args = find_callback(args, kw)
+        packetData = dumps(dict(name=event, args=args), ensure_ascii=False)
+        path = kw.get('path', '')
+        self.send_packet(5, path, packetData, callback)
+
+    def ack(self, packetID, *args):
+        packetID = packetID.rstrip('+')
+        packetData = '%s+%s' % (
+            packetID,
+            dumps(args, ensure_ascii=False),
+        ) if args else packetID
+        self.send_packet(6, data=packetData)
+
+    def set_messageCallback(self, callback):
+        'Set callback that will be called after receiving an acknowledgment'
+        self.messageID += 1
+        self.callbackByMessageID[self.messageID] = callback
+        return '%s+' % self.messageID
+
+    def get_messageCallback(self, messageID):
+        try:
+            callback = self.callbackByMessageID[messageID]
+            del self.callbackByMessageID[messageID]
+            return callback
+        except KeyError:
+            return
+
+    @property
+    def has_messageCallback(self):
+        return True if self.callbackByMessageID else False
+
+    def recv_packet(self):
+        try:
+            packet = self.connection.recv()
+        except websocket.WebSocketConnectionClosedException:
+            text = 'Lost connection (Connection closed)'
+            raise SocketIOConnectionError(text)
+        except socket.timeout:
+            text = 'Lost connection (Connection timed out)'
+            raise SocketIOConnectionError(text)
+        except socket.error, e:
+            text = 'Lost connection: %s' % e
+            raise SocketIOConnectionError(text)
+        #print ">>", packet, "<<"
+        try:
+            packetParts = packet.split(':', 3)
+        except AttributeError:
+            raise SocketIOPacketError('Received invalid packet (%s)' % packet)
+        packetCount = len(packetParts)
+        code, packetID, path, data = None, None, None, None
+        if 4 == packetCount:
+            code, packetID, path, data = packetParts
+        elif 3 == packetCount:
+            code, packetID, path = packetParts
+        elif 1 == packetCount:
+            code = packetParts[0]
+        return code, packetID, path, data
+
+    def send_packet(self, code, path='', data='', callback=None):
+        packetID = self.set_messageCallback(callback) if callback else ''
+        packetParts = [str(code), packetID, path, data]
+        try:
+            packet = ':'.join(packetParts)
+            self.connection.send(packet)
+        except socket.error:
+            raise SocketIOPacketError('Could not send packet')
+
+    @property
+    def connected(self):
+        return self.connection.connected
+
+
+class SocketIOError(Exception):
+    pass
+
+
+class SocketIOConnectionError(SocketIOError):
+    pass
+
+
+class SocketIOPacketError(SocketIOError):
+    pass
+
+
+def find_callback(args, kw=None):
+    'Return callback whether passed as a last argument or as a keyword'
+    if args and callable(args[-1]):
+        return args[-1], args[:-1]
+    try:
+        return kw['callback'], args
+    except (KeyError, TypeError):
+        return None, args
