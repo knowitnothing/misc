77050412dae145be773202f6e3f633ad98472b77
A <B>
2013-07-07 02:17:45 -0300

    stats.py is now meant to be executed by a scheduler

--- justdice/no_browser_bot/stats.py
+++ justdice/no_browser_bot/stats.py
@@ -2,18 +2,39 @@
 # Browserless Bot for collecting data from just-dice.com
 #
 
+import sys
 import time
+
 from browserless_driver import load_justdice, JustDiceSocket
 
 class GetStats(JustDiceSocket):
-    def __init__(self, *args):
+    def __init__(self, output, *args):
         super(GetStats, self).__init__(*args)
+        self.out = open(output, 'a')
+        self.fields = ['timestamp', 'bets', 'profit', 'purse', 'wagered']
+        self.got_result = False
+        self._working = False
 
     def on_result(self, result):
-        print result['stats']
-
+        if self._working:
+            return
+        self._working = True
+        stats = result['stats']
+        stats['timestamp'] = int(time.time())
+        line = ','.join(map(str, [stats[field] for field in self.fields]))
+        self.out.write("%s\n" % line)
+        self.got_result = True
 
 print "Connecting.."
 response = load_justdice()
-justdice = GetStats(response, None)
-justdice.sock.wait()
+sys.stdout.write("Waiting for result..")
+justdice = GetStats('out.csv', response, None)
+now = time.time()
+max_wait_time = 10 # seconds
+while not justdice.got_result and time.time() - now < max_wait_time:
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    time.sleep(0.1)
+print
+if not justdice.got_result:
+    print "No results this time"
